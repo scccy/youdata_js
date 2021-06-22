fc1f36c919840e1987fff2bc238c48adbf250f94
scccy <shici2880@gmail.com>
2021-06-22 15:19:35 +0800

    111

--- 饼图/南丁格尔.js
+++ 饼图/南丁格尔.js
@@ -1,55 +1,90 @@
+var data1 =dataSet.map(function(d) {return d[0];});
+var data2 =dataSet.map(function(d) {return d[1];});
+var data3 =dataSet.map(function(d) {return d[2];});
+
 option = {
+    tooltip:{
+        triggrt:'item',
+        formatter: function(p){
+            let value = p.value;
+            if(value >999){
+                let parts = value.toString().split('.');
+                parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g,',');
+                value = parts.join('.');
+            }
+            let res = p.seriesName = '<br/>' + p.name + ':'+value+'('+p.percent+'%)';
+            return res
+        }
+    },
     series: [
         {
+            name: '访问来源',
             type: 'pie',
             radius: '80%',
             center: ['50%', '50%'],
-            roseType: 'area',
-            itemStyle: {
-                borderRadius: 8
-            },
             data: [
-                {value: 1118, name: '搜索引擎',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#FED1B4' },
-                                    { offset: 1, color: '#FD99BD' }
-
-                                ]
-                            }
+                {value: data3, name: '票据',
+                    itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#FED1B4'},
+                                            {offset:1,color:'#FD99BD'}
+                                        ]
+                                    }
+                                }
                         }
-                    }},
-                {value: 1090, name: '直接访问',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#57BAF5' },
-                                    { offset: 1, color: '#12FFF7' }
-                                ]
-                            }
-                        }
-                    }
                 },
-                {value: 1200, name: '邮件营销',itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#4499FF' },
-                                    { offset: 1, color: '#99C9FF' }
-                                ]
-                            }
+                {value: data2, name: '理财',
+                    itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#57BAF5'},
+                                            {offset:1,color:'#12FFF7'}
+                                        ]
+                                    }
+                                }
                         }
-                    }}
-            ]
+                },
+                {value: data1, name: '存款',
+                    itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#4499FF'},
+                                            {offset:1,color:'#99c9FF'}
+                                        ]
+                                    }
+                                }
+                        }
+                }
+            ],
+            roseType: 'radius',
+            label: {
+                color: '#626f86'
+            },
+            labelLine: {
+                lineStyle: {
+                    color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+            },
+            itemStyle: {
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0)'
+            }
         }
     ]
 };
--- 饼图/双饼图.js
+++ 饼图/双饼图.js
@@ -1,86 +1,110 @@
-option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b}: {c} ({d}%)'
-    },
+var data1 =dataSet.map(function(d) {return d[0];});
+var data2 =dataSet.map(function(d) {return d[1];});
+var data3 =dataSet.map(function(d) {return d[2];});
+var data4 =dataSet.map(function(d) {return d[3];});
 
-    series: [
+option = {
+    tooltip:{
+        triggrt:'item',
+        formatter:'{b}:{c}({d}%)'
+    },
+    series:[
         {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['15%', '20%'],
-            itemStyle: {
-                borderRadius: 5,
-                borderColor: '#fff',
-                borderWidth: 2
+            type:'pie',
+            radius:['15%','25%'],
+            itemStyle:{
+                borderRadius:20,
+                // borderColor:'#fff',
+                borderWidth:4
             },
-            data: [
-                {value: 1000, name: '未到期理财',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#32437D' },
-                                    { offset: 1, color: '#32437C' }
-                                ]
-                            }
-                        }
-                    }},
-                {value: 500, name: '未到期理财',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#93D2FF' },
-                                    { offset: 1, color: '#2C89F6' }
-                                ]
-                            }
-                        }
-                    }},
-            ]
+            data:[
+                {value:data4,name:'融资未到期', itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#32437d'},
+                                            {offset:1,color:'#32437c'}
+                                        ]
+                                    }
+                                }
+                        }},
+                {value:data3,name:'到期融资', itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#93d2ff'},
+                                            {offset:1,color:'#2c89f6'}
+                                        ]
+                                    }
+                                }
+                        }},
+            ],
+            // labelLine: {
+            //     lineStyle: {
+            //         color: '#000000'
+            //     },
+            //     smooth: 0,
+            //     length:0,
+            //     length2: 0
+            // },
+            label:{
+                show:false
+            }
         },
         {
-            name: '访问来源',
-            type: 'pie',
-            radius: ['65%', '90%'],
-            itemStyle: {
-                borderRadius: 20,
-                borderColor: '#fff',
-                borderWidth: 2
+            type:'pie',
+            radius:['45%','60%'],
+            itemStyle:{
+                borderRadius:20,
+                // borderColor:'#fff',
+                borderWidth:4
             },
-            data: [
-                {value: 1000, name: '未到期理财',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#32437D' },
-                                    { offset: 1, color: '#32437C' }
-                                ]
-                            }
-                        }
-                    }},
-                {value: 500, name: '未到期理财',
-                    itemStyle: {
-                        normal: {
-                            color: {
-                                type: 'linear',
-                                colorStops: [
-                                    // !! 在此添加渐变过程色 !!
-                                    { offset: 0, color: '#FFC7C7' },
-                                    { offset: 1, color: '#FA3C7B' }
-                                ]
-                            }
-                        }
-                    }},
-
-            ]
+            data:[
+                {value:data2,name:'未到期理财', itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#32437d'},
+                                            {offset:1,color:'#32437c'}
+                                        ]
+                                    }
+                                }
+                        }},
+                {value:data1,name:'到期理财', itemStyle:
+                        {
+                            normal:
+                                {
+                                    color:{
+                                        type:"linear",
+                                        colorStops:[
+                                            {offset:0,color:'#ffc7c7'},
+                                            {offset:1,color:'#fa3c78'}
+                                        ]
+                                    }
+                                }
+                        }},
+            ],
+            // labelLine: {
+            //     lineStyle: {
+            //         color: '#000000'
+            //     },
+            //     smooth: 0,
+            //     length:0,
+            //     length2: 0
+            // },
+            label:{
+                show:false
+            }
         }
     ]
-};
+}
+  
